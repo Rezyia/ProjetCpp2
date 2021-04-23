241cfc32f514e24777a695b31ae89d05e13e684a
Rezyia <fineladiesprotectress@gmail.com>
2021-04-23 11:03:27 +0200

    ajout commentaires CArc.h

--- ProjetCpp2/ProjetCpp2/CArc.h
+++ ProjetCpp2/ProjetCpp2/CArc.h
@@ -6,23 +6,26 @@ private:
 	unsigned int uiARCDestination;
 
 public:
+	// Constructeur par défaut
 	CArc(unsigned int uiArgDest) {
 		uiARCDestination = uiArgDest;
 	}
+	// Constructeur de recopie
 	CArc(CArc* ARCarg) {
 		uiARCDestination = ARCarg->ARCGetNumero();
 	}
 
+	// Fonction d'affichage du l'arc
 	void ARCAfficher() {
 		std::cout << "Arc vers " + uiARCDestination;
 	}
 
-	// Getter
+	// Getter : retourne le numéro de destination de l'arc
 	unsigned int ARCGetNumero() {
 		return uiARCDestination;
 	}
 
-	// Setter
+	// Setter : associe le numéro de destination de l'arc avec l'argument passé en paramètre
 	void ARCSetNumero(unsigned int uiArgNumDest) {
 		uiARCDestination = uiArgNumDest;
 	}
