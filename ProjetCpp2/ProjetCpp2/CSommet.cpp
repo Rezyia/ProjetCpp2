cd5ce78a885b9fdff953b131c9127680a4c8d212
Rezyia <fineladiesprotectress@gmail.com>
2021-04-23 11:31:44 +0200

    update CSommet

--- ProjetCpp2/ProjetCpp2/CSommet.cpp
+++ ProjetCpp2/ProjetCpp2/CSommet.cpp
@@ -1,1 +1,52 @@
 #include "CSommet.h"
+
+CSommet::CSommet() {
+	uiSOMNumero = -1;
+	uiSOMNbArrivants = 0;
+	uiSOMNbPartants = 0;
+	ppArcSomArrivants = (CArc**) malloc(sizeof(CArc*) * 0);
+	ppArcSomPartants = (CArc**)malloc(sizeof(CArc*) * 0);
+}
+
+
+CSommet::CSommet(unsigned int uiArg){
+	uiSOMNumero = uiArg;
+	uiSOMNbArrivants = 0;
+	uiSOMNbPartants = 0;
+	ppArcSomArrivants = (CArc**)malloc(sizeof(CArc*) * 0);
+	ppArcSomPartants = (CArc**)malloc(sizeof(CArc*) * 0);
+}
+
+
+CSommet::CSommet(CSommet* SOMarg){
+	uiSOMNumero = SOMarg->SOMGetNumero();
+	uiSOMNbPartants = SOMarg->SOMGetNbPartants();
+	uiSOMNbArrivants = SOMarg->SOMGetNbArrivants();
+
+	// Allocation des tableaux
+	ppArcSomArrivants = (CArc**)malloc(sizeof(CArc*) * uiSOMNbArrivants);
+	ppArcSomPartants = (CArc**)malloc(sizeof(CArc*) * uiSOMNbPartants);
+
+	// Recopie tableau arrivants
+	for (unsigned int uiBoucleCopie = 0; uiBoucleCopie < uiSOMNbArrivants; uiBoucleCopie++) {
+		// ppArcSomArrivants[uiBoucleCopie] = ... (à compléter)
+	}
+
+	// Recopie tableau partants
+	for (unsigned int uiBoucleCopie = 0; uiBoucleCopie < uiSOMNbPartants; uiBoucleCopie++) {
+		// ppArcSomPartants[uiBoucleCopie] = ... (à compléter)
+	}
+}
+
+
+CSommet::~CSommet(){
+	 for (unsigned int uiBoucleDel = 0; uiBoucleDel < uiSOMNbArrivants; uiBoucleDel++) {
+		 delete ppArcSomArrivants[uiBoucleDel];
+	 }
+	 for (unsigned int uiBoucleDel = 0; uiBoucleDel < uiSOMNbPartants; uiBoucleDel++) {
+		 delete ppArcSomPartants[uiBoucleDel];
+	 }
+	 free(ppArcSomArrivants);
+	 free(ppArcSomPartants);
+}
+
--- ProjetCpp2/ProjetCpp2/CSommet.h
+++ ProjetCpp2/ProjetCpp2/CSommet.h
@@ -1,5 +1,7 @@
 #include "CArc.h"
 
+
+
 #pragma once
 class CSommet
 {
@@ -7,23 +9,46 @@ private:
 	unsigned int uiSOMNumero;
 	CArc** ppArcSomPartants;
 	CArc** ppArcSomArrivants;
+	unsigned int uiSOMNbPartants;
+	unsigned int uiSOMNbArrivants;
 
 public:
+	// Constructeur par défaut
 	CSommet();
+
+	// Constructeur de recopie
 	CSommet(CSommet* SOMarg);
+
+	// Constructeur de confort ()
 	CSommet(unsigned int uiArg);
+
+	// Destructeur
+	~CSommet();
+
 
 	bool SOMIsEmptyArrivants();
 	bool SOMIsEmptyPartants();
 	void SOMAjouterArc(CSommet* pArgDestination); // Ajouter l'arc du sommet appelé vers la destination.
 	void SOMSupprimerArc(CSommet* pArgDestination); // Supprimer l'arc du sommet appelé vers la destination.
+	CArc* SOMGetArc(CSommet* pArgDestination); // Retoure l'arc du sommet vers la destination passée en paramètre.
 
-	// Getter
+
+	// Getter : retourne le numéro du sommet
 	unsigned int SOMGetNumero() {
 		return uiSOMNumero;
 	}
 
-	// Setter
+	// Getter : retourne le nombre d'arcs partants
+	unsigned int SOMGetNbPartants() {
+		return uiSOMNbPartants;
+	}
+
+	// Getter : retourne le nombre d'arcs arrivants
+	unsigned int SOMGetNbArrivants() {
+		return uiSOMNbArrivants;
+	}
+
+	// Setter : associe le numéro du sommet avec l'argument passé en paramètre
 	void SOMSetNumero(unsigned int uiArgNum) {
 		uiSOMNumero = uiArgNum;
 	}
